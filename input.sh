5bb5cc2d979089c6c2b08b43b1c9539737e9ff6c
root <root@ip-172-26-15-150.ec2.internal>
2023-09-24 21:34:11 +0000

    bash script samples

---
+++ input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+read -p "What is your name ?  " NAME 
+read -p "Are you taking the class with utrains? " c
+
+echo " your name is: $NAME and you answer $c to the utrains class taking"
+
